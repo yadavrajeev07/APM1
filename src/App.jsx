b96aba804fdc92b04793f0a747c81945ab624bcd
Rajeev Yadav <rajeevyadavraju7@gmail.com>
2026-07-19 18:12:28 +0530

    Full Code Update

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,9 @@
+import Topbar from "./components/layout/Topbar";
 import Footer from "./components/layout/Footer";
 import AppRoutes from "./routes/AppRoutes";
 import WhatsAppButton from "./components/WhatsAppButton";
 import Navbar from "./components/layout/Navbar";
-import Topbar from "./components/layout/Topbar";
+
 
 export default function App() {
   return (
